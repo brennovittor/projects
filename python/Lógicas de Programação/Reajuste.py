ce1da3de1ffc44810868e2c3c042aacb220c5183
clym1z <alvesbrenno281@gmail.com>
2024-10-03 17:57:35 -0300

    new algoritm

---
+++ python/Lógicas de Programação/Reajuste.py
@@ -0,0 +1,6 @@
+#2  Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%.
+
+valor = int(input('Diga um número inteiro: '))
+reajuste = valor + (valor * 0.05)
+
+print(reajuste)
