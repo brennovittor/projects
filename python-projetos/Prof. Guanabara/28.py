098f0477ef069f499dbb6d613973d52bb066f336
Brenno <147351941+brennovittor@users.noreply.github.com>
2024-12-28 15:40:25 -0300

    Create 28.py

---
+++ python-projetos/Prof. Guanabara/28.py
@@ -0,0 +1,11 @@
+import random
+
+while True:
+    n1 = random.randint(0, 5)
+    n2 = int(input('Em qual número eu pensei?'))
+
+    if n2 == n1:
+        print('Está correto')
+        break
+    else:
+        print('Tente novamente')
